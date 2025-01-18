acb13b66ba1dfe4d20d3dcceb6fc0b5cc5ee3411
muhammadfahadamir <amirkhankahout@gmail.com>
2025-01-18 16:42:15 +0500

    multivarconcept

---
+++ variableconcepts.py
@@ -0,0 +1,19 @@
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
