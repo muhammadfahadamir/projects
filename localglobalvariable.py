8f1c2d325c0a12baccdb3d889de0caa3a9938cd3
muhammadfahadamir <amirkhankahout@gmail.com>
2025-01-20 16:05:08 +0500

    local and global concept

---
+++ localglobalvariable.py
@@ -0,0 +1,20 @@
+x = "awesome"
+
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+
+
+print("Python is " + x)
+
+myfunc()
+myfunc()
+myfunc()
+
+def glob():
+  global y
+  y = "fantabulous"
+
+glob()
+
+print("Python is " + y)
