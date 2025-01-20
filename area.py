98e307992be5dfba27189ad1008aa0145dd8b0b6
muhammadfahadamir <amirkhankahout@gmail.com>
2025-01-20 17:07:59 +0500

    area of triangle , area of square and tempurature convertion of farhanheight and  celcius

---
+++ area.py
@@ -0,0 +1,7 @@
+x = 12
+y = 13
+r = 14
+m = Area_of_trianlge = .5*x*y
+n =Area_of_square = 3.14*r*r
+print("Area of triangle is", m)
+print("Area of squate is" , n)
---
+++ cir_far_cel.py
@@ -0,0 +1,7 @@
+celcius = float(input("Enter tempurature in celcius?"))
+fahranheight = (celcius * 1.8) + 32
+print("tempurature in farenheight is " , fahranheight)
+
+farhanheight = float(input("Enter tempurature in farhenheight?"))
+celcius = (farhanheight - 32)/1.8
+print("tempurature in celcius is " , celcius)
