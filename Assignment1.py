8328aef96353e9baab114dbb6177933acdece643
muhammadfahadamir <amirkhankahout@gmail.com>
2025-01-24 17:04:34 +0500

    test1

---
+++ Assignment1.py
@@ -0,0 +1,50 @@
+x = 10.3,y = 12
+z = int(x)
+print(z)
+
+
+a = 15,b = 4
+print("adittion:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("modulus:",a%b)
+print("floor division",a&b)
+print("exponenttial:",a**b)
+
+num1=8 ,num2=5
+print(num1 > num2)
+print(num1 == num2)
+print(num1 != num2)
+print(num1 <= num2)
+print(num1 >= num2)
+
+x = True,y = False
+print(x and y)
+print(x or y)
+print(not x)
+print(not y)
+
+a= 10,b=5.5,c='20'
+d=float(a)+float(b)
+f=float(a)*float(c)
+g=c+'apples'
+print(d)
+print(f)
+print(g)
+
+x=12,y=7,z=15
+print(x+y>z)
+
+x=10,y=5,z=0
+print(x > y and y > z)
+print(x < y or z == 0)
+print(not x == 10)
+
+a=18,b=12
+print(a > b and b > 5)
+print(a == b or b < 15)
+
+print((5+10)*2-(8/4)+(15%4))
+print("congragulation")
+
