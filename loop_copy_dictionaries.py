5e7790583e9dcfcf308ef466240d5ff6f95be71c
muhammadfahadamir <amirkhankahout@gmail.com>
2025-02-18 16:17:59 +0500

    loop and copy dictionaries

---
+++ loop_copy_dictionaries.py
@@ -0,0 +1,28 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x)
+  
+for x in thisdict:
+  print(thisdict[x])
+  
+for x in thisdict.values():
+  print(x)
+  
+for x in thisdict.keys():
+  print(x)
+
+for x, y in thisdict.items():
+  print(x, y)
+  
+  
+  
+  
+mydict = thisdict.copy()
+print(mydict)
+
+mydict2 = dict(thisdict)
+print(mydict2)
